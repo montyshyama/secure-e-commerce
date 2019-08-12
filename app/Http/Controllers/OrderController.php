a00fe105ffeffe62d2be8878a590a3065b817003
montyshyama <montyshyama031@gmail.com>
2019-08-12 19:51:29 +0530

    Orders and Relations

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Order;
 
 class OrderController extends Controller
 {
     public function index() {
 
-    	return view('admin.orders.index');
+    	$orders = Order::all();
+    	return view('admin.orders.index', compact('orders'));
+
     }
 
 }
